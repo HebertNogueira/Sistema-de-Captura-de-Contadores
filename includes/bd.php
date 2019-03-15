2c63ebd2360fc3c5a03546fbccdb895ef92b8774
Hebert Nogueira <nogueira.hebert@gmail.com>
2019-03-15 14:15:13 -0300

    Corrigido arquivo BD

---
+++ includes/bd.php
@@ -0,0 +1,14 @@
+﻿<?php
+	class connect {
+		public function connecting(){
+			global $mysqli;
+			//Alterar LOGIN e SENHA
+			$mysqli = new mysqli('127.0.0.1', '>>>login<<<', '>>>senha<<<', 'printers');
+			
+			if (mysqli_connect_errno()) {
+				die('Não foi possível conectar-se ao banco de dados, motivo: ' . mysqli_connect_error() . ".");
+				exit();
+			}			
+		}	
+	}
+?>
